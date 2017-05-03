b616a20b038a1d9c2e3b8cd0017152b97bd9476a
renebrugger <rene.brugger@gmx.ch>
2017-05-03 09:30:17 +0200

    priv(sound): test mp3

--- index.vr.js
+++ index.vr.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import {
-  AppRegistry,
-  asset,
-  StyleSheet,
-  Pano,
-  Text,
-  View,
+    AppRegistry,
+    asset,
+    StyleSheet,
+    Pano,
+    Text,
+    View,
 } from 'react-vr';
 
 export default class virtualInsurance extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('office.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Pano source={asset('surfing.jpg')}/>
+                <Text
+                    style={{
+                        backgroundColor: '#777879',
+                        fontSize: 0.8,
+                        fontWeight: '400',
+                        layoutOrigin: [0.5, 0.5],
+                        paddingLeft: 0.2,
+                        paddingRight: 0.2,
+                        textAlign: 'center',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [0, 0, -3]}],
+                    }}>
+
+                </Text>
+                <Sound
+                    source={{
+                        mp3: asset('ambient.mp3'),
+                    }}
+                />
+            </View>
+        );
+    }
 };
 
 AppRegistry.registerComponent('virtualInsurance', () => virtualInsurance);
