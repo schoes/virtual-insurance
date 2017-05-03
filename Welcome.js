070c5f0c2beee7299341714611bd472fca9da6a0
Sven Schoeni <sven.schoeni@gmail.com>
2017-05-03 16:25:51 +0200

    dudeli du

--- Welcome.js
+++ Welcome.js
@@ -8,26 +8,30 @@ export default class WelcomeScreen extends React.Component {
 
     constructor() {
         super();
-        this.styles = {
-            welcome: {
-                fontSize: 0.8,
-                fontWeight: '400',
-                layoutOrigin: [0.5, 0.5],
-                paddingLeft: 0.2,
-                paddingRight: 0.2,
-                textAlign: 'center',
-                textAlignVertical: 'center',
-                transform: [{translate: [0, 0, -3]}],
-            },
-            hausrat: {
-                fontSize: 0.8,
-                fontWeight: '400',
-                layoutOrigin: [1.5, 1.5],
-                paddingLeft: 0.2,
-                paddingRight: 0.2,
-                textAlign: 'center',
-                textAlignVertical: 'center',
-                transform: [{translate: [-2, 0, 0]}, {rotateY: 45}],
+
+        this.state = {
+            styles: {
+                welcome: {
+                    fontSize: 0.8,
+                    fontWeight: '400',
+                    layoutOrigin: [0.5, 0.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [{translate: [0, 0, -3]}],
+                    textColor: 'white'
+                },
+                hausrat: {
+                    fontSize: 0.8,
+                    fontWeight: '400',
+                    layoutOrigin: [1.5, 1.5],
+                    paddingLeft: 0.2,
+                    paddingRight: 0.2,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    transform: [{translate: [-2, 0, 0]}, {rotateY: 45}],
+                }
             }
         }
     }
@@ -36,21 +40,60 @@ export default class WelcomeScreen extends React.Component {
         this.setState({site: PAGE_HAUSRAT})
     }
 
+    changeColor = () => {
+        if (this.state.styles.welcome.textColor === 'white') {
+            this.setState({
+                styles: {
+                    welcome: {
+                        textColor: 'blue'
+                    }
+                }
+            })
+        }
+        else {
+            this.setState({
+                styles: {
+                    welcome: {
+                        textColor: 'white'
+                    }
+                }
+            })
+        }
+
+    }
+
     render = () => {
         return (
             <View>
                 <Pano source={asset('maxresdefault.jpg')}/>
                 <Sound source={{mp3: asset('ambient.mp3')}}/>
-                <Text
-                    style={this.styles.welcome}>Willkommen</Text>
+                <Text onEnter={() => {
+                    this.setState({
+                        styles: {
+                            welcome: {
+                                textColor: 'white'
+                            }
+                        }
+                    })
+                }}
+                      onExit={() => {
+                          this.setState({
+                              styles: {
+                                  welcome: {
+                                      textColor: 'blue'
+                                  }
+                              }
+                          })
+                      }}
+                      style={this.state.styles.welcome}>Willkommen</Text>
                 <VrButton
-                    style={{width: 0.7}}
-                    onClick={this.naviagteToIsurance}>
+                    style={this.state.styles.hausrat}
+                    onClick={this.naviagteToIsurance()}>
                     <Image source={asset("home.png")}>
-                        <Text style={this.styles.hausrat}>
-                            Hausratsversicherung
-                        </Text>
                     </Image>
+                    <Text style={this.state.styles.hausrat}>
+                        Hausratsversicherung
+                    </Text>
                 </VrButton>
             </View>
 
