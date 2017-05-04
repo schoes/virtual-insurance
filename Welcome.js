2bed4a6f3ce30d409edd6672e096e3283ca1e7d7
Sven Schoeni <sven.schoeni@gmail.com>
2017-05-04 09:52:24 +0200

    juhui

--- Welcome.js
+++ Welcome.js
@@ -10,94 +10,81 @@ export default class WelcomeScreen extends React.Component {
         super();
 
         this.state = {
-            styles: {
-                welcome: {
-                    fontSize: 0.8,
-                    fontWeight: '400',
-                    layoutOrigin: [0.5, 0.5],
-                    paddingLeft: 0.2,
-                    paddingRight: 0.2,
-                    textAlign: 'center',
-                    textAlignVertical: 'center',
-                    transform: [{translate: [0, 0, -3]}],
-                    textColor: 'white'
-                },
-                hausrat: {
-                    fontSize: 0.8,
-                    fontWeight: '400',
-                    layoutOrigin: [1.5, 1.5],
-                    paddingLeft: 0.2,
-                    paddingRight: 0.2,
-                    textAlign: 'center',
-                    textAlignVertical: 'center',
-                    transform: [{translate: [-2, 0, 0]}, {rotateY: 45}],
-                }
-            }
+            textColor: 'blue',
+            infoTextVisibility: 'none'
         }
     }
 
-    naviagteToIsurance = () => {
-        this.setState({site: PAGE_HAUSRAT})
-    }
-
-    changeColor = () => {
-        if (this.state.styles.welcome.textColor === 'white') {
-            this.setState({
-                styles: {
-                    welcome: {
-                        textColor: 'blue'
-                    }
-                }
-            })
+    _toggleInfoText = () => {
+        if (this.state.infoTextVisibility === 'none') {
+            this.setState({infoTextVisibility: 'flex'});
         }
         else {
-            this.setState({
-                styles: {
-                    welcome: {
-                        textColor: 'white'
-                    }
-                }
-            })
+            this.setState({infoTextVisibility: 'none'});
         }
-
+        console.log('test button click event');
     }
 
     render = () => {
         return (
             <View>
                 <Pano source={asset('maxresdefault.jpg')}/>
-                <Sound source={{mp3: asset('ambient.mp3')}}/>
-                <Text onEnter={() => {
-                    this.setState({
-                        styles: {
-                            welcome: {
-                                textColor: 'white'
-                            }
-                        }
-                    })
-                }}
-                      onExit={() => {
-                          this.setState({
-                              styles: {
-                                  welcome: {
-                                      textColor: 'blue'
-                                  }
-                              }
-                          })
-                      }}
-                      style={this.state.styles.welcome}>Willkommen</Text>
-                <VrButton
-                    style={this.state.styles.hausrat}
-                    onClick={this.naviagteToIsurance()}>
-                    <Image source={asset("home.png")}>
-                    </Image>
-                    <Text style={this.state.styles.hausrat}>
-                        Hausratsversicherung
-                    </Text>
-                </VrButton>
+                <View>
+                    <Sound source={{mp3: asset('ambient.mp3')}}/>
+                </View>
+                <View style={{
+                    layoutOrigin: [0, 0, 0],
+                    transform: [{translate: [0, 0, 0]}, {rotateX: 0}, {rotateY: 0}, {rotateZ: 0}],
+                }}>
+                    <Text style={{
+                        backgroundColor: '#FFF',
+                        fontSize: 0.8,
+                        color: 'red',
+                        layoutOrigin: [0.5, 0.5],
+                        paddingLeft: 0.2,
+                        paddingRight: 0.2,
+                        textAlign: 'center',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [0, 0, -5]}]
+                    }}>Mitte</Text>
+                </View>
+                <View style={{
+                    layoutOrigin: [0, 0, 0],
+                    width: 3, height: 3,
+                    borderColor: 'black',
+                    transform: [{translate: [0, 0, 0]}, {rotateX: 0}, {rotateY: 90}, {rotateZ: 0}],
+                }}>
+                    <VrButton style={{
+                        layoutOrigin: [0.5, 0.5],
+                        transform: [{translate: [0, 0.5, -5]}]
+                    }}
+                              onHeadPoseCaptured={() => this._toggleInfoText()}
+                              onExit={() => this._toggleInfoText()}>
+
+                        <Image source={asset('home.png')}
+                               style={{width: 1, height: 1}}/>
+
+                        <View style={{display: this.state.infoTextVisibility}}>
+                            <Text style={{
+                                fontSize: 0.2,
+                                opacity: 0.6,
+                                padding: 0.2,
+                                width: 3,
+                                height: 1.5,
+                                margin: 0.5,
+                                backgroundColor: 'white'
+                            }}>
+                                Schliessen Sie keine Hausratsversicherung bei der Mobiliar ab.
+                            </Text>
+                        </View>
+                    </VrButton>
+                </View>
             </View>
 
 
-        );
+
+
+        )
+            ;
     };
 }
