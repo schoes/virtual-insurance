b77463cdbe7e8f7f7b20b5692a4d10ef93ac4deb
pascalgraf <pascal.graf@gmx.net>
2017-05-04 14:55:58 +0200

    aisjdhfjiasdhf

--- Flipchart.js
+++ Flipchart.js
@@ -19,32 +19,88 @@ export default class Flipchart extends React.Component {
                            height: 5.1,
                            layoutOrigin: [0, 0],
                            transform: [{translate: [-2.8, 2.1, -10]}]
-                       }}
-                >
+                       }}>
+
                     <Text style={{
+                        width: 8,
+                        height: 2,
                         fontSize: 0.5,
                         color: 'white',
-                        layoutOrigin: [0.5, 0.5],
+                        layoutOrigin: [0, 0],
                         textAlign: 'center',
                         textAlignVertical: 'center',
-                        transform: [{translate: [1, -3, -5]}]
+                        transform: [{translate: [-2.8, 2.1, -10]}]
                     }}>
-                        VIRTUAL INSURANCE
+                        ** VIRTUAL INSURANCE **
+                    </Text>
 
+                    <Text style={{
+                        width: 8,
+                        height: 1,
+                        fontSize: 0.5,
+                        color: 'white',
+                        layoutOrigin: [0, 0],
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [-2.3, 2.1, -10]}]
+                    }}>
                         -APPROACH:
                          set up dummy project
+                    </Text>
+                    <Text style={{
+                        width: 8,
+                        height: 1,
+                        fontSize: 0.5,
+                        color: 'white',
+                        layoutOrigin: [0, 0],
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [-2.3, 2.1, -10]}]
+                    }}>
                         -RESULT:
                          virtual mobi office
+                    </Text>
+                    <Text style={{
+                        width: 8,
+                        height: 1,
+                        fontSize: 0.5,
+                        color: 'white',
+                        layoutOrigin: [0, 0],
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [-2.3, 2.1, -10]}]
+                    }}>
                         -DEMO:
                          grab the vr goggle
+                    </Text>
+                    <Text style={{
+                        width: 8,
+                        height: 2,
+                        fontSize: 0.5,
+                        color: 'white',
+                        layoutOrigin: [0, 0],
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [-2.3, 2.1, -10]}]
+                    }}>
                         -SUMMARY:
                          young and buggy
                          technology
+                    </Text>
+                    <Text style={{
+                        width: 8,
+                        height: 1,
+                        fontSize: 0.5,
+                        color: 'white',
+                        layoutOrigin: [0, 0],
+                        textAlign: 'left',
+                        textAlignVertical: 'center',
+                        transform: [{translate: [-2.3, 2.1, -10]}]
+                    }}>
                         -RECOMMENDATION:
                          PO organizes
                          cash to realize
                          the project
-
                     </Text>
                 </Image>
 
